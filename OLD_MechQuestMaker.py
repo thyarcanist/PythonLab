5d54fb150a7d7bb37936128766b7c6d12052a69c
voidespy <48234494+voidespy@users.noreply.github.com>
2021-02-09 01:57:13 -0500

    Add files via upload

---
+++ OLD_MechQuestMaker.py
@@ -0,0 +1,32 @@
+#Datorien Anderson and Jake Logsdon What is YOUR AGE? PROMPT #5 4/22/2014 5th
+mech=input ("What is your mechs name? ")
+attributes=input ("What does you mech look like? ")
+name=input ("What is your name? ")
+age=input ("What is your age? ")
+pID=input ("Enter your Pilot Identification number. ")
+mem1=input("Who is the first member of your squad? ")
+mem2=input ("Who is the second member of your squad? ")
+mem3=input("Who is the third member of your squad? ")
+item1=input("A plural noun. ")
+item2=input("A plural noun. ")
+item3=input("A noun. ")
+item4=input ("An adjective. ")
+item5=input ("Another adjective. ")
+item6=input("Another plural noun. ")
+insult=input ("Please enter an insult. ")
+
+
+print('You are the pilot of the ' +mech+ ', ' +attributes+ ' which is currently in repair. ')
+print('You... ' +name+ ' are ' +age+ ' years of age.')
+print('Your pilot ID number is ' +pID+ '. It is highly recommened that you do not lose this information')
+
+
+print('Urgent news!')
+
+
+print('Teammates ' +mem1+ ', ' +mem2+ ', and ' +mem3+ ' are highly appreciative of your abilities.')
+print('It seems that ' +mem2+ ' has found your stashe of ' +item6+ ', we will get that back for you ASAP.')
+print('So, ' +mem1+ ' and ' +mem3+ ' have made multiple '  +item4+ ' , ' +item2+ ' please contain them.')
+print('We cannot stress how valuable your teammates are, especially ' +mem1+ ' okay? ' )
+print('Im sorry to say but '+mem3+ ' is of lesser importance so please... get that ' +item5+ ', ' +insult+ ' out of here! okay?')
+
